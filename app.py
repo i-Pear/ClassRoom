572f6672f389760f961852ee9a925af5b139f2e7
iPear <i.pear@outlook.com>
2019-11-11 21:30:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,24 @@ from flask import Flask, request, url_for, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
 import json
 import time
+import urllib.request
+
+WECHAT_APPID = ""
+WECHAT_APPSECRET = ""
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 # key：教室名称如 一号楼A302 类型string
 # value：教室状态如 000000000000 类型int
 room_occupy = {"2019/10/01": {"一号楼A302": 0}}
+
+
+def getOpenID(code):
+    url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=%s&secret=%s&code=%s&grant_type=authorization_code" % (
+        WECHAT_APPID, WECHAT_APPSECRET, code)
+    response = urllib.request.urlopen(url)
+    resp_dict = json.loads(response.read())
+    return resp_dict["openid"]
 
 
 # dashboard series
@@ -30,6 +42,7 @@ def getOccupy():
 # 返回网页 （借哪个教室）
 @app.route('/submit')
 def submit():
+    openid = getOpenID(str(request.args["code"]))
     return render_template("submit.html")
 
 
@@ -42,6 +55,7 @@ def submitResult():
 # 订单 渲染
 @app.route('/record')
 def record():
+    openid = getOpenID(str(request.args["code"]))
     return 'Hello World!'
 
 
