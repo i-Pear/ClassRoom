38b81015f132168e967da352d3c1a4c0ff4ffcf7
iPear <i.pear@outlook.com>
2019-11-11 17:59:22 +0800

    Update entity.py

--- entity.py
+++ entity.py
@@ -1,29 +1,27 @@
 from app import db
+import datetime
 
-class Entry(db.Model):
+
+class RequestEntry(db.Model):
     __tablename__ = "requests"
 
-    id = db.Column(db.INTEGER, primary_key=True, nullable=False)
+    id = db.Column(db.INTEGER, primary_key=True, nullable=False, autoincrement=1)
     stuid = db.Column(db.INTEGER, index=True, nullable=False)
     classroom = db.Column(db.VARCHAR(20), index=True, nullable=False)
-    segment = db.Column(db.VARCHAR(32), index=True, nullable=False)
-    requestTime = db.Column(db.DATE, unique=True, nullable=False)
-    requestReason = db.Column(db.VARCHAR(500), nullable=True)
-    choice = db.Column(db.VARCHAR(256), nullable=False)
-    has_file = db.Column(db.BOOLEAN, nullable=False, default=False)
+    date = db.Column(db.INTEGER, nullable=False)
+    segment = db.Column(db.INTEGER, index=True, nullable=False)
+    requestTime = db.Column(db.DATETIME, nullable=False, default=datetime.datetime.now)
+    requestReason = db.Column(db.VARCHAR(500), default="")
+    status = db.Column(db.INTEGER, default=0)
 
-    def __init__(self, username: str, realname: str, academy: str,
-                 clazz: str, phone: str, qq: str, choice: str, has_file: bool):
-        self.username = username
-        self.realname = realname
-        self.academy = academy
-        self.clazz = clazz
-        self.phone = phone
-        self.qq = qq
-        self.choice = choice
-        self.has_file = has_file
+    def __init__(self, id: int, stuid: int, classroom: str, date: int, segment: int, requestReason: str, ):
+        self.id = id
+        self.stuid = stuid
+        self.classroom = classroom
+        self.date = date
+        self.segment = segment
+        self.requestReason = requestReason
 
     def __repr__(self):
-        return f"<Entry of user {self.username}: {self.realname} @ {self.clazz} of {self.academy} Tel {self.phone} " \
-               f"Choice {self.choice} HasFile {self.has_file}>"
-
+        return f"<Entry of request {self.id}: stuid {self.stuid} requested classroom {self.classroom} on {self.date} segment {self.segment} " \
+               f" requestTime:{self.requestTime} requestReason:{self.requestReason} status:{self.status}"
