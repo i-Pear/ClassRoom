69eb69d7649b6c3136def8d7e474e2cde2a7bb97
Waitti <44718627+Waitti-long@users.noreply.github.com>
2019-11-11 17:22:58 +0800

    nanan

--- app.py
+++ app.py
@@ -1,36 +1,48 @@
-from flask import Flask,request
+from flask import Flask, request
 
 app = Flask(__name__)
 
+
 # dashboard series
+# 教室占用网页
 @app.route('/dashboard')
 def dashboard():
     return 'Hello World!'
 
+
+# 根据日期返回教室占用
 @app.route('/getOccupy')
 def getOccupy():
     return 'Hello World!'
 
+
 # submit series
+# 返回网页 （借哪个教室）
 @app.route('/submit')
 def submit():
     return 'Hello World!'
+
 
 @app.route('/submitResult')
 def submitResult():
     return 'Hello World!'
 
-#record series
+
+# record series
+# 订单
 @app.route('/record')
 def record():
     return 'Hello World!'
 
+
+# 撤回
 @app.route('/withdraw')
 def withdraw():
     return 'Hello World!'
 
+
 @app.route('/')
-def ():
+def home():
     return 'Hello World!'
 
 
