2c4de3599cdc6236747d561e9c33d16dc63fb63d
Waitti <44718627+Waitti-long@users.noreply.github.com>
2019-11-11 21:11:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, url_for, render_template, redirect
 from flask_sqlalchemy import SQLAlchemy
+from entity import *
 import json
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 import init_db
+
 # key：教室名称如 一号楼A302 类型string
 # value：教室状态如 000000000000 类型int
 room_occupy = {"2019/10/01": {"一号楼A302": 0}}
@@ -36,7 +38,15 @@ def submit():
 
 @app.route('/submitResult')
 def submitResult():
-    return 'Hello World!'
+    jd = request.json
+    date = jd["date"]
+    # TODO 这里要对date做校验,至于怎样算是一个合法的日期输入呢？我们不得而知
+
+    request_time = datetime.datetime.now()
+    req = RequestEntry(int(jd["stuid"]), jd["classroom"], date, jd["segment"], str(request_time))
+    db.session.add(req)
+    db.session.commit()
+    return 'OK!'
 
 
 # record series
