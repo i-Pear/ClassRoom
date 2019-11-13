22813b95fca73970bfc9683f80ce11f888b3d07f
Waitti <44718627+Waitti-long@users.noreply.github.com>
2019-11-13 16:33:54 +0800

    s

--- args_format.py
+++ args_format.py
@@ -11,6 +11,7 @@ with open(r'E:\Softs\Projects\pycharmProjects\ClassRoom\builds.txt', "r", encodi
         build = f.readline()
 
 
+# 允许的日期为八位数字或字符串
 def is_allowed_date(date):
     try:
         datetime.datetime.strptime(str(date), "%Y%m%d")
@@ -19,6 +20,7 @@ def is_allowed_date(date):
         return False
 
 
+# 允许的房间名格式为 建筑楼_A301 建筑和房间按照一定格式写在builds.txt里面
 def is_allowed_room(room):
     room = str(room)
     ros = room.split("_")
